ff7b65a5e1daa2879cb0ca65ca19ef96296bbe94
Shusha <shrifffsan@gmail.com>
2024-05-23 16:18:02 +0800

    modified:   assets/css/style.css new file:   assets/fonts/Dudka Bold.ttf new file:   assets/img/ico/close.png new file:   assets/img/ico/info.png modified:   assets/scripts/changeLang.js modified:   assets/scripts/script.js modified:   index.html

--- assets/scripts/changeLang.js
+++ assets/scripts/changeLang.js
@@ -23,6 +23,10 @@ const langData = {
         "en": "Rules",
         "ru": "Правила",
     },
+    "rulesTxt": {
+        "en": "The game consists of two rounds: first as a pack of Chupacabras, then as the shepherd. The player who has more sheep left alive wins.<br><br>1. chupacabras round:<br>- Choose three chupacabra tokens (their identity is hidden from the opponent).<br>- Choose a sheep you want to kill, and announce your choice. The chosen sheep dies.<br>- All tokens adjacent to the killed sheep light up and must be moved to free cells. You cannot choose a place for the chupacabra among the killed sheep, as they are not highlighted.<br><br>2. Shepherd round:<br>- You are shown which sheep was killed.<br>- You need to guess which of your sheep was the hidden chupacabra.<br>- If you guess correctly, the chupacabra escapes. If not, your sheep dies.<br>- The round ends when all Chupacabras leave the field or all sheep are killed.<br><br>Good luck in the game!",
+        "ru": "Игра состоит из двух раундов: сначала в качестве стаи чупакабр, затем в качестве пастуха. Побеждает игрок, у которого в живых осталось больше овец.<br><br> 1. Раунд чупакабр:<br> - Выберите три фишки чупакабр (их идентичность скрыта от противника).<br> - Выберите овцу, которую хотите убить, и объявите свой выбор. Выбранная овца погибает.<br> - Все фишки, находящиеся рядом с убитой овцой, подсвечиваются и должны быть передвинуты на свободные клетки. Нельзя выбирать место для чупакабр среди убитых овец, так как они не подсвечиваются.<br><br>2. Раунд пастуха:<br> - Вам показывается, какая овца была убита.<br>- Вам нужно угадать, какая из ваших овец была скрытой чупакаброй.<br>- Если вы угадываете правильно, чупакабра убегает. Если нет, ваша овца погибает.<br> - Раунд завершается, когда все чупакабры покинули поле или когда все овцы убиты.<br><br>Удачи в игре!",
+    },
 };
 
 var elements = document.getElementsByClassName("chL");
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,14 @@
-window.onbeforeunload = function() {
-	return true;
-};
+//window.onbeforeunload = function() {
+	//return true;
+//};
 
 const newGameBtn = document.getElementById('newGame');
-const modal = document.getElementById('modal');
+const startMenu = document.getElementById('startMenu');
+const main = document.getElementById('main');
 
 newGameBtn.onclick = function() {
-    modal.style.display = "none";
+    startMenu.style.display = "none";
+    main.style.display = "grid";
 }
 
 //Menu (w < 960)
@@ -20,4 +22,33 @@ navBtn.onclick = function() {
         navBtn.value = "cl";
         navv.style.display = "none";
     }
+}
+
+const rulBtn = document.getElementById('rulBtn');
+const rulBtn2 = document.getElementById('rulBtn2');
+const rules = document.getElementById('rules');
+
+rulBtn.addEventListener("click", rulOpen);
+rulBtn2.addEventListener("click", rulOpen);
+
+function rulOpen() {
+    rules.style.display = "grid";
+}
+
+
+const closeRulBtn = document.getElementById('closeRulBtn');
+closeRulBtn.onclick = function() {
+    rules.style.display = "none";
+}
+
+const infoBtn = document.getElementById('infoBtn');
+const info = document.getElementById('info');
+infoBtn.onclick = function() {
+    info.style.display = "grid";
+}
+
+
+const closeInfoBtn = document.getElementById('closeInfoBtn');
+closeInfoBtn.onclick = function() {
+    info.style.display = "none";
 }
